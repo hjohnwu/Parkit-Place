2bd2c487023e19ad35184c4ec0b0a4f851403c98
RitvikDeshpande <deshpr3@mcmaster.ca>
2024-11-03 21:29:57 -0500

    SCRUM-18: Configuration updates (i.e. version)

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -2,11 +2,14 @@
 
 const functions = require("firebase-functions");
 const express = require("express");
-
 const app = express();
 
-const test = require("./test");
+// Import routes
+const test = require("./routes/test");
+const chat = require("./routes/chat");
 
+// Use routes
 app.use("/test", test);
+app.use("/chat", chat);
 
 exports.api = functions.https.onRequest(app);
